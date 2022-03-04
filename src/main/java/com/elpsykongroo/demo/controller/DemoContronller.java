601b4156278156f3dcad3bec4275d3300818f121
Labman4 <50023562+Labman4@users.noreply.github.com>
2022-03-04 23:30:01 +0800

    kubernetes discovery

--- src/main/java/com/elpsykongroo/demo/controller/DemoContronller.java
+++ src/main/java/com/elpsykongroo/demo/controller/DemoContronller.java
@@ -1,15 +1,19 @@
 package com.elpsykongroo.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
 public class DemoContronller {
-    @GetMapping("/api")
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("")
     public String access() {
       return "success";
     }
