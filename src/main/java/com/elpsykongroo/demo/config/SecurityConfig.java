91fe2374f9faddcbd76ef5ba837bab373187f6c5
Labman4 <50023562+Labman4@users.noreply.github.com>
2022-12-29 00:07:06 +0800

    add test & disable csrf

--- src/main/java/com/elpsykongroo/demo/config/SecurityConfig.java
+++ src/main/java/com/elpsykongroo/demo/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.cors().and()
+		  .  csrf().disable()
 //				.requiresChannel(channel ->
 //						channel.anyRequest().requiresSecure())
 				.authorizeHttpRequests((authorize) -> authorize
--- src/main/java/com/elpsykongroo/demo/controller/IPManagerContronller.java
+++ src/main/java/com/elpsykongroo/demo/controller/IPManagerContronller.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
 @RequestMapping("/ip")
 @Slf4j
