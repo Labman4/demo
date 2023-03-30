e33f4028c7d6670cb73911b53521e862a0ced235
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-03-30 20:17:58 +0800

    test cookie cors

--- auth/authorization-server/src/main/java/com/elpsykongroo/auth/server/config/WebConfig.java
+++ auth/authorization-server/src/main/java/com/elpsykongroo/auth/server/config/WebConfig.java
@@ -3,13 +3,8 @@ package com.elpsykongroo.auth.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.web.http.DefaultCookieSerializer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
 
 @Configuration(proxyBeanMethods = false)
 public class WebConfig {
@@ -29,29 +24,31 @@ public class WebConfig {
     }
 
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-        config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addAllowedOrigin("http://127.0.0.1:15173");
-        config.addAllowedOrigin("https://elpsykongroo.com");
-        config.addAllowedOrigin("https://auth-dev.elpsykongroo.com");
-        config.addAllowedOrigin("https://oauth2-proxy2.elpsykongroo.com");
-        config.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOriginPatterns(Arrays.asList("*"));
+//        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+//        config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        config.addAllowedOrigin("http://127.0.0.1:15173");
+//        config.addAllowedOrigin("https://elpsykongroo.com");
+//        config.addAllowedOrigin("https://login.elpsykongroo.com");
+//        config.addAllowedOrigin("https://auth-dev.elpsykongroo.com");
+//        config.addAllowedOrigin("https://oauth2-proxy2.elpsykongroo.com");
+//        config.setAllowCredentials(true);
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
 
     @Bean
     public DefaultCookieSerializer defaultCookieSerializer() {
         DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
         defaultCookieSerializer.setCookiePath("/");
         defaultCookieSerializer.setSameSite(null);
+        defaultCookieSerializer.setDomainNamePattern("^(([^.]+)\\.)?(elpsykongroo\\.com|localhost)$");
         return defaultCookieSerializer;
     }
 }
