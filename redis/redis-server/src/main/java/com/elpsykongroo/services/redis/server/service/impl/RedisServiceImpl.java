3b589e25b562d3be68a93af9e67eddd65885aefd
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-04-06 09:51:29 +0800

    add log

--- redis/redis-server/src/main/java/com/elpsykongroo/services/redis/server/service/impl/RedisServiceImpl.java
+++ redis/redis-server/src/main/java/com/elpsykongroo/services/redis/server/service/impl/RedisServiceImpl.java
@@ -1,23 +1,28 @@
 package com.elpsykongroo.services.redis.server.service.impl;
 
 import com.elpsykongroo.services.redis.server.service.RedisService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@Slf4j
 public class RedisServiceImpl implements RedisService {
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
     @Override
     public void setCache(String key, String value) {
-           redisTemplate.opsForValue().set(key, value);
+        log.debug("set cache with k-v: {} -> {}", key, value );
+        redisTemplate.opsForValue().set(key, value);
     }
 
     @Override
     public String getCache(String key) {
+        log.debug("get cache with k: {}", key);
         Object result = redisTemplate.opsForValue().get(key);
+        log.debug("get cache result: {} --> {}", key, result == null ? "" : result.toString());
         return result == null ? "" : result.toString();
     }
 }
