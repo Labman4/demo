a9abba27c27a25107029180cf98f4aa9db4775e0
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-05-30 01:28:44 +0800

    add vault to manage es cred

--- elasticsearch/elasticsearch-server/src/main/java/com/elpsykongroo/services/elasticsearch/server/config/ElasticConfig.java
+++ elasticsearch/elasticsearch-server/src/main/java/com/elpsykongroo/services/elasticsearch/server/config/ElasticConfig.java
@@ -20,19 +20,26 @@ import com.elpsykongroo.base.config.ServiceConfig;
 import com.elpsykongroo.services.elasticsearch.server.utils.SSLUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.vault.annotation.VaultPropertySource;
 
 import javax.net.ssl.SSLContext;
 import java.time.Duration;
 
+@ConditionalOnProperty(
+        prefix = "service",
+        name = "vault",
+        havingValue = "true",
+        matchIfMissing = false)
+@VaultPropertySource(value = "${SECRETS_PATH:database/creds/elastic-internal}")
 @Configuration(proxyBeanMethods = false)
 @EnableElasticsearchRepositories("com.elpsykongroo.services.elasticsearch.server.repo")
 public class ElasticConfig extends ElasticsearchConfiguration {
-
     @Autowired
     private ServiceConfig serviceConfig;
 
@@ -41,12 +48,14 @@ public class ElasticConfig extends ElasticsearchConfiguration {
     
    @Override
    public ClientConfiguration clientConfiguration() {
+       String username = env.getProperty("username");
+       String password = env.getProperty("password");
        ServiceConfig.ES es =  serviceConfig.getElastic();
        if ("public".equals(es.getSsl().getType())) {
            return ClientConfiguration.builder()
                     .connectedTo(es.getNodes())
                     .usingSsl()
-                    .withBasicAuth(env.getProperty("service.elastic.user"), env.getProperty("service.elastic.pass"))
+                    .withBasicAuth(username, password)
                     .withConnectTimeout(Duration.ofSeconds(es.getTimeout().getConnect()))
                     .withSocketTimeout(Duration.ofSeconds(es.getTimeout().getSocket()))
                     .build();
@@ -56,14 +65,14 @@ public class ElasticConfig extends ElasticsearchConfiguration {
            return ClientConfiguration.builder()
                     .connectedTo(es.getNodes())
                     .usingSsl(sslContext)
-                    .withBasicAuth(env.getProperty("service.elastic.user"), env.getProperty("service.elastic.pass"))
+                    .withBasicAuth(username, password)
                     .withConnectTimeout(Duration.ofSeconds(es.getTimeout().getConnect()))
                     .withSocketTimeout(Duration.ofSeconds(es.getTimeout().getSocket()))
                     .build();
        } else if (StringUtils.isNotEmpty(es.getUser())){
            return ClientConfiguration.builder()
                     .connectedTo(es.getNodes())
-                    .withBasicAuth(env.getProperty("service.elastic.user"), env.getProperty("service.elastic.pass"))
+                    .withBasicAuth(username, password)
                     .withConnectTimeout(Duration.ofSeconds(es.getTimeout().getConnect()))
                     .withSocketTimeout(Duration.ofSeconds(es.getTimeout().getSocket()))
                     .build();
