03740abbda38f130e81ad7329f5471e45378b88c
Labman4 <50023562+Labman4@users.noreply.github.com>
2023-09-20 19:58:17 +0800

    fix deleteByIP,csrfFilter

--- base/src/main/java/com/elpsykongroo/base/optional/filter/CsrfCookieFilter.java
+++ base/src/main/java/com/elpsykongroo/base/optional/filter/CsrfCookieFilter.java
@@ -16,19 +16,19 @@
 
 package com.elpsykongroo.base.optional.filter;
 
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public final class CsrfCookieFilter extends OncePerRequestFilter {
+public class CsrfCookieFilter implements Filter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
         // Render the token value to a cookie by causing the deferred token to be loaded
