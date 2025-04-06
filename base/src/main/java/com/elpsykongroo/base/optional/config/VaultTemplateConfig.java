0310eb458c6e5dbcbfbb65cd90a07c6bf81fe995
Labman4 <50023562+Labman4@users.noreply.github.com>
2025-04-06 16:38:58 +0800

    fix

--- base/src/main/java/com/elpsykongroo/base/optional/config/VaultTemplateConfig.java
+++ base/src/main/java/com/elpsykongroo/base/optional/config/VaultTemplateConfig.java
@@ -35,7 +35,7 @@ import java.net.URI;
     name = "vault",
     havingValue = "true",
     matchIfMissing = false)
-@VaultPropertySource(value = "${SECRETS_APP_BASE_PATH:kv/app/dev/base}", renewal = VaultPropertySource.Renewal.RENEW))
+@VaultPropertySource(value = "${SECRETS_APP_BASE_PATH:kv/app/dev/base}", renewal = VaultPropertySource.Renewal.RENEW)
 @VaultPropertySource("${SECRETS_APP_PATH}")
 public class VaultTemplateConfig extends AbstractVaultConfiguration  {
 
